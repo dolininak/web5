98008084919a5bedaa8cdb3c46a6a8aec50d1184
dolininak <144163170+dolininak@users.noreply.github.com>
2023-10-15 14:10:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const btnResultNode=document.querySelector('.js-btn-result');
 const outputNode=document.querySelector('.js-output');
 var konf=document.getElementById('konf');
 
+
 konf.addEventListener('change',function(){
     listener(konf);
 });
@@ -12,9 +13,13 @@ kol.addEventListener('change',function(){
 function listener(elem){
     var cena=elem.querySelector(':checked').getAttribute('data-price'),
     kol1=kol.value;
-
+   
     totalPrice=cena*kol1;
 }
 btnResultNode.addEventListener('click',function(){
-    outputNode.innerHTML=totalPrice;
+    var regex = /^\d+$/;
+    if (!regex.test(totalPrice)) {
+        outputNode.innerHTML = "Недопустимые символы";
+      } else {
+    outputNode.innerHTML="ИТОГО:"+totalPrice;}
 });
